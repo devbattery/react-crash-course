abaad5732ba8f95256e38a5bfdedc6d7b148d911
devbattery <devbattery@outlook.com>
2025-09-19 21:09:40 +0900

    Feat: 상태 이용을 위한 준비

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost.jsx";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author={"Wonjun"} body={"React.js is awesome!!"} />
-      <Post author={"Anna"} body={"Next.js is also awesome!!"} />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author={"Wonjun"} body={"React.js is awesome!!"} />
+        <Post author={"Anna"} body={"Next.js is also awesome!!"} />
+      </ul>
+    </>
   );
 }
 
