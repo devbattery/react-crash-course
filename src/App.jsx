f44978eef2f101b8307c560e7d0137fd27d36fae
devbattery <devbattery@outlook.com>
2025-09-19 20:39:15 +0900

    Feat: 컴포넌트 재사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Post from "./components/Post.jsx";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
 }
 
 export default App;
