87deab0da67324dc916e81908c9d760acb48e2bc
devbattery <devbattery@outlook.com>
2025-09-20 14:55:34 +0900

    Feat: 이전 State를 기반으로 State 갱신

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,13 +2,19 @@ import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
 import Modal from "./Modal.jsx";
+import { useState } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
 
