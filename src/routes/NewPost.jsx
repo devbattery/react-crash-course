d591124423bc1319b27a8284bb8130fdf69fd61d
devbattery <devbattery@outlook.com>
2025-09-20 20:35:15 +0900

    Feat: 경로 연결 및 경로 탐색

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,9 @@
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 import Modal from "../components/Modal.jsx";
+import { Link } from "react-router-dom";
 
-function NewPost({ onCancel, onAddPost }) {
+function NewPost({ onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -21,7 +22,6 @@ function NewPost({ onCancel, onAddPost }) {
       author: enteredAuthor,
     };
     onAddPost(postData);
-    onCancel();
   }
 
   return (
@@ -41,9 +41,9 @@ function NewPost({ onCancel, onAddPost }) {
           />
         </p>
         <p className={classes.actions}>
-          <button type={"button"} onClick={onCancel}>
+          <Link to={".."} type={"button"}>
             Cancel
-          </button>
+          </Link>
           <button>Submit</button>
         </p>
       </form>
