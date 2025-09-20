e08b4902dd4b9cd4855474ae5295281d44676312
devbattery <devbattery@outlook.com>
2025-09-20 15:02:44 +0900

    Feat: 목록 데이터 표시

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,8 +1,8 @@
-import Post from "./Post.jsx";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost.jsx";
 import Modal from "./Modal.jsx";
 import { useState } from "react";
+import Post from "./Post.jsx";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
@@ -18,9 +18,19 @@ function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        <Post author={"Anna"} body={"I'm hungry!"} />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yest.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
